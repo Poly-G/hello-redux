b29af7d9c15863ba460637c591ed8f859d081827
Poly Goveia <poly.goveia@gmail.com>
2019-06-22 10:14:44 -0700

    add store to the app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
